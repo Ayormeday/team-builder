63e49cb57df775e4bcbce8107c7d8559f9bbc916
Ayomide Jones <ayormeday@gmail.com>
2019-09-18 17:05:35 +0100

    styling via app.css

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,22 +1,24 @@
-import React, { useState }from 'react';
-import './App.css';
-import uuid from 'uuid';
-import Form from './Form';
-
+import React, { useState } from "react";
+import "./App.css";
+import uuid from "uuid";
+import Form from "./Form";
 
 const initialTeamMemberList = [
-  { id: uuid(), name: 'gabe', email:'a@yahoo.com', role: 'Full stack dev'},
-  { id: uuid(), name: 'luke', email: 'b@getMaxListeners.com', role: 'Full stack dev'},
-  { id: uuid(), name: 'josh', email: 'c@yahoo.com', role: 'Full stack dev'},
-
+  { id: uuid(), name: "gabe", email: "a@yahoo.com", role: "Full stack dev" },
+  {
+    id: uuid(),
+    name: "luke",
+    email: "b@getMaxListeners.com",
+    role: "Full stack dev"
+  },
+  { id: uuid(), name: "josh", email: "c@yahoo.com", role: "Full stack dev" }
 ];
 
 const initialTeamMemberForm = {
-  name: '',
-  email: '',
-  role : '',
+  name: "",
+  email: "",
+  role: ""
 };
-
 
 function App() {
   const [teamMemberList, setTeamMemberList] = useState(initialTeamMemberList);
@@ -28,61 +30,63 @@ function App() {
   //    [e.target.id]: e.target.value
   //   });
   //   };
- 
 
   const onNameChange = e => {
     setTeamMemberForm({
       name: e.target.value,
       email: teamMemberForm.email,
-      role: teamMemberForm.role,
-    })
-  }
+      role: teamMemberForm.role
+    });
+  };
   const onEmailChange = e => {
     setTeamMemberForm({
       name: teamMemberForm.name,
       email: e.target.value,
-      role: teamMemberForm.role,
-    })
-  }
+      role: teamMemberForm.role
+    });
+  };
   const onRoleChange = e => {
     setTeamMemberForm({
       name: teamMemberForm.name,
       email: teamMemberForm.email,
-      role: e.target.value,
-    })
-  }
+      role: e.target.value
+    });
+  };
 
-
-    const onFormSubmit = e => { 
-      e.preventDefault();
-      const newTeamMember = {
-        id: uuid(),
-        name: teamMemberForm.name,
-        email: teamMemberForm.email,
-        role: teamMemberForm.role
-      };
-      const newteamMemberList = teamMemberList.concat(newTeamMember);
-      setTeamMemberList(newteamMemberList);
-      setTeamMemberForm(initialTeamMemberForm);
+  const onFormSubmit = e => {
+    e.preventDefault();
+    const newTeamMember = {
+      id: uuid(),
+      name: teamMemberForm.name,
+      email: teamMemberForm.email,
+      role: teamMemberForm.role
     };
-
+    const newteamMemberList = teamMemberList.concat(newTeamMember);
+    setTeamMemberList(newteamMemberList);
+    setTeamMemberForm(initialTeamMemberForm);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <Form 
+      <Form
         onNameChange={onNameChange}
         onEmailChange={onEmailChange}
         onRoleChange={onRoleChange}
         onFormSubmit={onFormSubmit}
         teamMemberForm={teamMemberForm}
-        />
-        {/* {
-        teamMemberList.map(member => {
-          <h5 key={member.id}> {member.name} {member.email} {member.role}</h5>
-        })
-        } */}
-      </header>
+      />
+      {
+        teamMemberList.map(member => (
+          <div className='display' key={member.id}>
+            <h3 >
+              {member.name}
+            </h3>
+            <h5> {member.role}</h5>
+            <p>{member.email} </p>
+          </div>
+        ))
+      }
+    
     </div>
   );
 }
